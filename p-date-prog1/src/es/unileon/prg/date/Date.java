8b093298e20bd1566a75eff80ae9ee65b8e7aeba
arubir01 <arubir01.estudiantes.unileon.es>
2019-03-21 12:52:46 +0100

    Continuacion de la practica

--- p-date-prog1/src/es/unileon/prg/date/Date.java
+++ p-date-prog1/src/es/unileon/prg/date/Date.java
@@ -6,15 +6,42 @@ package es.unileon.prg.date;
 */
 public class Date {
 
-	private int _day;
-	private int _month;
-	private int _year;
+	private int day;
+	private int month;
+	private int year;
 
 	public Date(int day, int month, int year) throws DateException{
-		this._year = year;
+		this.year = year;
 		this.setMonth(month);
 		this.setDay(day);
 		}
+private String MonthsLeftOfYear(Date anotherDate)
+{
+	for(int n = this.month;((n >= 1) && (n <= 12));n++)
+{
+	StringBuilder.append(getNameOfMonth(n) + "\n");		
+}
+public boolean isSameDate(Date anotherDate)
+{
+	return ((isSameDay(anotherDate)) && (isSameMonth(anotherDate)) && 			 			(isSameYear(anotherDate)));
+}
+
+public boolean isSameDay(Date anotherDate)
+{
+	return (this.day == anotherDate.getDay());
+}
+
+public boolean isSameMonth(Date anotherDate)
+{
+	return (this.month == anotherDate.getMonth());
+}
+
+public boolean isSameYear(Date anotherDate)
+{
+	return (this.year == anotherDate.getYear());
+}
+
+
 private int getDaysOfMonth(int month)
 	{
 	int number = 0;
@@ -36,28 +63,97 @@ private int getDaysOfMonth(int month)
 	case 2:
 	number = 28;
 	break;
-	default:
-	number = -1;
 	}
 	return number;
 	}
+
+private String getNameOfMonth(int month)
+	{
+	String mes = "";
+	switch (month) {
+	case 1: mes = "Enero";
+	break;
+	case 3: mes = "Marzo";
+	break;
+	case 5: mes = "Mayo";
+	break;
+	case 7: mes = "Julio";
+	break;
+	case 8: mes = "Agosto";
+	break;
+	case 10: mes = "Octubre";
+	break;
+	case 12: mes = "Diciembre";
+	break;
+	case 4: mes = "Abril";
+	break;
+	case 6: mes = "Junio";
+	break;
+	case 9: mes = "Septiembre";
+	break;
+	case 11: mes = "Noviembre";
+	break;
+	case 2: mes = "Febrero";
+	break;
+
+	}
+	return mes;
+	}
+private String getSeasonOfMonth(int month)
+	{
+	String estacion = "";
+	switch (month) {
+	case 12://next
+	case 1: //next
+	case 2: estacion = "Invierno";
+	break;
+	case 3: //next
+	case 4: //next
+	case 5: estacion = "Primavera";
+	break;
+	case 6: //next
+	case 7: //next
+	case 8: estacion = "Verano";
+	break;
+	case 9: //next 
+	case 10: //next
+	case 11: estacion = "Otoño";
+	break;
+	}
+	return estacion;
+	}
+public int getDay()
+{
+	return this.day;
+}
+
+public int getMonth()
+{
+	return this.month;
+}
+
+public int getYear()
+{
+	return this.year;
+}
+
 public void setMonth(int month) throws DateException
 	{
 	if (month < 1 || month > 12) {
 			throw new DateException("Mes " + month + " no valido" +
-					" Valores posibles entre 1 y 12.");
+					" Valores posibles entre 1 y" + getMonth() + ".");
 		} 
 		else{
-			this._month = month;
+			this.month = month;
 		}
 }
 public void setDay(int day) throws DateException
 	{
-	if (day < 1 || day > getDaysOfMonth(_month)) {
+	if (day < 1 || day > getDaysOfMonth(month)) {
 			throw new DateException("Dia " + day + " no valido" +
-					" Valores posibles entre 1 y" + getDaysOfMonth(_month) 						+ ".");
+					" Valores posibles entre 1 y" + getDaysOfMonth(month) 						+ ".");
 		} else {
-			this._day = day;
+			this.day = day;
 		}
 /*
 *Boolean para is same tal
@@ -74,7 +170,7 @@ public void setDay(int day) throws DateException
 */
 @Override
 	public String toString() {
-		return this._day + "/" + this._month + "/" + this._year;
+		return this.day + "/" + this.month + "|" + this.getNameOfMonth(month) + "/" + this.year + "\nEstación: " + 		  			getSeasonOfMonth(month);
 }
 }
 
