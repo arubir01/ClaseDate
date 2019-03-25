5eac7161892b39b20650c88514b351a013b6d749
arubir01 <47630365+arubir01@users.noreply.github.com>
2019-03-26 00:14:21 +0100

    Falta el ejercicio de fechas aleatorias

--- p-date-prog1/src/es/unileon/prg/date/Date.java
+++ p-date-prog1/src/es/unileon/prg/date/Date.java
@@ -10,20 +10,130 @@ public class Date {
 	private int month;
 	private int year;
 
-	public Date(int day, int month, int year) throws DateException{
+	public Date(int day, int month, int year) throws DateException
+		{
 		this.year = year;
 		this.setMonth(month);
 		this.setDay(day);
 		}
-private String MonthsLeftOfYear(Date anotherDate)
+
+private String getMonthsLeftOfYear(int month)
 {
-	for(int n = this.month;((n >= 1) && (n <= 12));n++)
-{
-	StringBuilder.append(getNameOfMonth(n) + "\n");		
+	StringBuilder string = new StringBuilder();
+	string.append("\nMeses restantes del año: ");
+	string.append(getNameOfMonth(this.month));
+	string.append("\n");
+	
+	for(int n = this.month + 1; n <= 12; n++)
+	{
+	string.append(getNameOfMonth(n));
+	string.append(" ");
 }
+return string.toString();	
+}
+
+private String getDaysUntilMonth()
+{
+	StringBuilder string2 = new StringBuilder();
+	string2.append("\nDias restantes del mes: ");
+	string2.append("\n");
+	
+	for(int m = this.day + 1; m <= getDaysOfMonth(month); m++)
+	{
+	
+	string2.append(m);
+	string2.append("/");
+	string2.append(month);
+	string2.append("/");
+	string2.append(year);
+	string2.append(" ");
+}
+return string2.toString();
+}
+
+
+private String getMonthsWithSameDays()
+{
+	StringBuilder string3 = new StringBuilder();
+	string3.append("\nMeses con los mismos dias: ");
+	string3.append("\n");
+
+	for(int m = 0; m <= 12; m++)
+	{
+	if(getDaysOfMonth(month) == getDaysOfMonth(m))
+{
+	string3.append(getNameOfMonth(m));
+	string3.append(" ");
+}
+}
+	return string3.toString();
+}
+
+private int getDaysUntilYear()
+{
+
+	int daysNumbers = this.day;
+	for(int n = 0; n < this.month; n++)
+{
+	daysNumbers += getDaysOfMonth(this.month);
+}	
+	return daysNumbers;
+}
+private String getWeek(int getDaysOfMonth)
+{
+	String week = " ";
+	switch(getDaysOfMonth)
+{
+	case 1: //next
+	case 8: //next
+	case 15: //next
+	case 22: //next
+	case 30: 
+	week = "Martes";
+	break;
+	case 2: //next
+	case 9: //next
+	case 16: //next
+	case 23: //next
+	case 31:
+	week = "Miercoles";
+	break;
+	case 3: //next
+	case 10: //next
+	case 17:  //next
+	case 24: 
+	week = "Jueves";
+	break;
+	case 4: //next
+	case 11: //next
+	case 18: //next
+	case 25:
+	week = "Viernes";
+	break;
+	case 5: //next
+	case 12: //next
+	case 19: //next
+	case 26:
+	week = "Sabado";
+	break;
+	case 6: //next
+	case 13: //next
+	case 20: //next
+	case 27:
+	week = "Domingo";
+	break;
+	case 7: //next
+	case 14: //next
+	case 21: //next
+	case 28:
+	week = "Lunes";
+	break;
+}
+	return week;
+}	
 public boolean isSameDate(Date anotherDate)
 {
-	return ((isSameDay(anotherDate)) && (isSameMonth(anotherDate)) && 			 			(isSameYear(anotherDate)));
+	return ((isSameDay(anotherDate)) && (isSameMonth(anotherDate)) && (isSameYear(anotherDate)));
 }
 
 public boolean isSameDay(Date anotherDate)
@@ -42,7 +152,7 @@ public boolean isSameYear(Date anotherDate)
 }
 
 
-private int getDaysOfMonth(int month)
+public int getDaysOfMonth(int month)
 	{
 	int number = 0;
 	switch (month) {
@@ -99,6 +209,7 @@ private String getNameOfMonth(int month)
 	}
 	return mes;
 	}
+
 private String getSeasonOfMonth(int month)
 	{
 	String estacion = "";
@@ -122,6 +233,7 @@ private String getSeasonOfMonth(int month)
 	}
 	return estacion;
 	}
+
 public int getDay()
 {
 	return this.day;
@@ -147,19 +259,21 @@ public void setMonth(int month) throws DateException
 			this.month = month;
 		}
 }
+
 public void setDay(int day) throws DateException
 	{
 	if (day < 1 || day > getDaysOfMonth(month)) {
 			throw new DateException("Dia " + day + " no valido" +
-					" Valores posibles entre 1 y" + getDaysOfMonth(month) 						+ ".");
+					" Valores posibles entre 1 y" + getDaysOfMonth(month) + ".");
 		} else {
 			this.day = day;
 		}
+}
 /*
 *Boolean para is same tal
 *return (this._month == month)
 */
-}
+
 
 
 
@@ -170,7 +284,6 @@ public void setDay(int day) throws DateException
 */
 @Override
 	public String toString() {
-		return this.day + "/" + this.month + "|" + this.getNameOfMonth(month) + "/" + this.year + "\nEstación: " + 		  			getSeasonOfMonth(month);
+		return this.day + "/" + this.month + "|" + this.getNameOfMonth(month) + "/" + this.year + "\nEstación: " + this.getSeasonOfMonth(month) + this.getMonthsLeftOfYear(month) + this.getDaysUntilMonth() + this.getMonthsWithSameDays() + "\nDias hasta la fecha: " + getDaysUntilYear() + "\nDia de la semana: " + getWeek(day);
 }
 }
-
